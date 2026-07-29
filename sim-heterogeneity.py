77249e2b944db0f7195540ae5fac00fb91dc6599
Bill Majoros <bmajoros@dcc-login-03.oit.duke.edu>
2026-07-29 17:45:20 -0400

    update

--- sim-heterogeneity.py
+++ sim-heterogeneity.py
@@ -23,16 +23,21 @@ class Donor:
     def __init__(self):
         self.genotypes=[]
 
+class Pool:
+    def __init__(self):
+        self.donors=[]
+
+def initPools(numPools):
+    return [Pool() for _ in range(numPools)]
+        
 def initDonors(numDonors):
-    donors=[]
-    for i in range(numDonors):
-        donors.append(Donor())
-    return donors
+    return [Donor() for _ in range(numDonors)]
         
 def loadVCF(vcfFilename,maxVariants,minAF,maxAF):
     numLoaded=0
     donors=None
     with gzip.open(vcfFilename,"rt") as IN:
+        # For each variant:
         for line in IN:
             if(line[0]=="#"): continue
             fields=line.rstrip().split()
@@ -40,28 +45,45 @@ def loadVCF(vcfFilename,maxVariants,minAF,maxAF):
             if(numFields<100): continue
             rex.findOrDie("AF=([^;]+);",fields[7])
             AF=float(rex[1])
-            if(AF<minAF or AF>maxAF): continue
+            if(AF<minAF or AF>maxAF): continue # allele frequency filter
             numLoaded+=1
             if(numLoaded>=maxVariants): break
             numDonors=numFields-9
             if(donors is None): donors=initDonors(numDonors)
+            # Append genotypes of this variant to each donor
             for i in range(9,numFields):
                 gt=fields[i]
                 rex.findOrDie("(\d)\|(\d)",gt)
                 gt=[rex[1],rex[2]]
                 donors[i-9].genotypes.append(gt)
     return donors
-                    
+
+def assignDonorsToPools(donors,pools):
+    numPools=len(pools)
+    for index, element in enumerate(donors):
+        pools[index % numPools].donors.append(element)
+
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=5):
-    exit(ProgramName.get()+" <in.vcf.gz> <max-variants> <min-AF> <max-AF>\n")
-(vcfFilename,maxVariants,minAF,maxAF)=sys.argv[1:]
+if(len(sys.argv)!=6):
+    exit(ProgramName.get()+" <in.vcf.gz> <max-variants> <min-AF> <max-AF> <num-pools>\n")
+(vcfFilename,maxVariants,minAF,maxAF,numPools)=sys.argv[1:]
 
+# Parse command line
 maxVariants=int(maxVariants)
 minAF=float(minAF)
 maxAF=float(maxAF)
-donors=loadVCF(vcfFilename,maxVariants,minAF,maxAF)
-print(donors[0].genotypes)
+numPools=int(numPools)
 
+# Load VCF file
+donors=loadVCF(vcfFilename,maxVariants,minAF,maxAF)
+numDonors=len(donors)
+#print(donors[0].genotypes)
+
+# Assign donors to pools
+pools=initPools(numPools)
+assignDonorsToPools(donors,pools)
+#for pool in pools: print(len(pool.donors))
+
+# Simulate counts
