48f8de05771f74feb76c62a07e3e52dad8a3d6a4
Bill Majoros <bmajoros@dcc-login-03.oit.duke.edu>
2026-08-03 12:37:43 -0400

    update

--- pool-specific-thetas.py
+++ pool-specific-thetas.py
@@ -8,24 +8,27 @@ import ProgramName
 
 from EssexParser import EssexParser
 
+NUM_POOLS=20
+
 def processEssex(filename,globalThetas):
     parser=EssexParser(filename)
     while(True):
         variant=parser.nextElem()   # returns root of the tree
         if(variant is None): break
         ID=variant.getAttribute("id")
-        pools=variant.getChildren("pool")
+        pools=variant.findChildren("pool")
         thetas=[]
         for pool in pools:
-            poolNum=pool[0]
+            poolNum=int(pool[0])
             poolTheta=getPoolTheta(pool)
+            poolTheta=round(poolTheta,5)
             thetas.append([poolNum,poolTheta])
         emit(ID,thetas,globalThetas)
     parser.close()
 
 def getPoolTheta(pool):
-    dnaNodes=pool.getChildren("DNA")
-    rnaNodes=pool.getChildren("RNA")
+    dnaNodes=pool.findChildren("DNA")
+    rnaNodes=pool.findChildren("RNA")
     (dnaRef,dnaAlt)=getCountSums(dnaNodes)
     (rnaRef,rnaAlt)=getCountSums(rnaNodes)
     theta=(rnaAlt/rnaRef)/(dnaAlt/dnaRef)
@@ -43,20 +46,34 @@ def getCountSums(nodes):
     return (normalizedRef,normalizedAlt)
         
 def emit(varID,thetas,globalThetas):
+    poolArray=["NA"]*NUM_POOLS
+    for pair in thetas:
+        (index,theta)=pair
+        poolArray[index-1]=str(theta)
     globalTheta=globalThetas[varID]
     print(varID,globalTheta,sep="\t",end="")
-    for theta in thetas:
-        print
-    
+    for i in range(NUM_POOLS):
+        print("\t",poolArray[i],sep="",end="")
+    print()
+
+def loadGlobalThetas(filename,MIN_P_REG):
+    thetas=dict()
+    with open(filename,"rt") as IN:
+        for line in IN:
+            fields=line.rstrip().split()
+            varID=fields[0]; theta=float(fields[1]); Preg=float(fields[4])
+            thetas[varID]=theta if Preg>=MIN_P_REG else 1.0
+    return thetas
 
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=2):
-    exit(ProgramName.get()+" <input.essex> <estimates.txt>\n")
-(essexFilename,estimatesFilename)=sys.argv[1:]
+if(len(sys.argv)!=4):
+    exit(ProgramName.get()+" <input.essex> <estimates.txt> <min-Preg>\n")
+(essexFilename,estimatesFilename,MIN_P_REG)=sys.argv[1:]
+MIN_P_REG=float(MIN_P_REG)
 
-globalThetas=loadGlobalThetas(estimatesFilename)
+globalThetas=loadGlobalThetas(estimatesFilename,MIN_P_REG)
 processEssex(essexFilename,globalThetas)
 
 
