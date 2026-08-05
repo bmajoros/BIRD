fbc4faa7584d68b7e4eed197ab3501ddaedcada6
Bill Majoros <bmajoros@dcc-login-03.oit.duke.edu>
2026-08-05 16:30:17 -0400

    update

--- sim-dropout.py
+++ sim-dropout.py
@@ -5,31 +5,88 @@
 #=========================================================================
 import sys
 import gzip
+import statistics
+from scipy.stats import binom
 import ProgramName
 from Rex import Rex
 
 rex=Rex()
 
-def processVCF(filename,MAX_VARIANTS,NUM_DONORS):
+def processVCF(filename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE):
     numProcessed=0
+    sims=[]
     with gzip.open(filename,"rt") as IN:
         for line in IN:
-            if(len(line<1)): continue
+            if(len(line)<1): continue
             if(line[0]=='#'): continue
+            genotypes=processLine(line,NUM_DONORS)
+            if(genotypes is None): continue
+            if(countAlts(genotypes)==0): continue
+            dropoutProbs=getDropoutProbs(genotypes,NUM_DONORS,MAX_POOLS,
+                                         COVERAGE)
+            sims.append(dropoutProbs)
+            numProcessed+=1
+            if(numProcessed>=MAX_VARIANTS): break
+    n=len(sims)
+    for numPools in range(MAX_POOLS):
+        print(probs[numPools])
+        ave=statistics.mean([probs[numPools] for probs in sims])
+        print(numPools,ave,"\t")
+
+def getDropoutProbs(genotypes,NUM_DONORS,MAX_POOLS,COVERAGE):
+    probs=[]
+    for numPools in range(1,MAX_POOLS+1):
+        partitioning=partition(genotypes,numPools)
+        p=getDropout(partitioning,COVERAGE)
+        probs.append([numPools,p])
+    print("PROBS=",probs)
+    print()
+    return probs
+
+def countAlts(pool):
+    return sum([gt[0]+gt[1] for gt in pool])
+
+def getDropout(pools,COVERAGE):
+    product=1
+    for pool in pools:
+        N=len(pool)*2
+        A=countAlts(pool)
+        freq=float(A)/float(N)
+        p=binom.pmf(0,N,freq)
+        product*=p
+    return product
+
+def partition(genotypes,NUM_POOLS):
+    pools=[[] for _ in range(NUM_POOLS)]
+    for i, gt in enumerate(genotypes):
+        pools[i % NUM_POOLS].append(gt)
+    return pools
             
+def processLine(line,NUM_DONORS):
+    fields=line.rstrip().split()
+    if(len(fields)<9+NUM_DONORS): return None
+    genotypes=fields[9:(9+NUM_DONORS)]
+    GT=[]
+    for genotype in genotypes:
+        rex.findOrDie("(\d)\|(\d)",genotype)
+        gt=[int(rex[1]),int(rex[2])]
+        GT.append(gt)
+    return GT
             
 #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  HG00096
 
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=4):
-    exit(ProgramName.get()+" <in.vcf.gz> <max-variants> <#donors>\n")
-(vcfFilename,MAX_VARIANTS,NUM_DONORS)=sys.argv[1:]
+if(len(sys.argv)!=6):
+    exit(ProgramName.get()+" <in.vcf.gz> <max-pools> <max-variants> <#donors> <coverage>\nwhere coverage is the average # fragments per position\n")
+(vcfFilename,MAX_POOLS,MAX_VARIANTS,NUM_DONORS,COVERAGE)=sys.argv[1:]
+MAX_POOLS=int(MAX_POOLS)
 MAX_VARIANTS=int(MAX_VARIANTS)
 NUM_DONORS=int(NUM_DONORS)
+COVERAGE=int(COVERAGE)
 
-processVCF(vcfFilename,MAX_VARIANTS,NUM_DONORS)
+processVCF(vcfFilename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE)
 
 
 
