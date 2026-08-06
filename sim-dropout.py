2b16bfa122da26b6b4a4482307a4bd5d0859b1c5
Bill Majoros <bmajoros@dcc-login-03.oit.duke.edu>
2026-08-06 07:13:11 -0400

    fixed coverage in binom

--- sim-dropout.py
+++ sim-dropout.py
@@ -11,9 +11,11 @@ from scipy.stats import binom
 import ProgramName
 from Rex import Rex
 
+DEBUG=0
 rex=Rex()
 
-def processVCF(filename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE):
+def processVCF(filename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE,
+               MIN_AF,MAX_AF):
     numProcessed=0
     sims=[]
     with gzip.open(filename,"rt") as IN:
@@ -22,18 +24,25 @@ def processVCF(filename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE):
             if(line[0]=='#'): continue
             genotypes=processLine(line,NUM_DONORS)
             if(genotypes is None): continue
-            if(countAlts(genotypes)==0): continue
+            AF=getAF(genotypes)
+            if(AF==0.0): continue
+            if(AF<MIN_AF or AF>MAX_AF): continue
+            #print("AF=",AF)
             dropoutProbs=getDropoutProbs(genotypes,NUM_DONORS,MAX_POOLS,
                                          COVERAGE)
             sims.append(dropoutProbs)
             numProcessed+=1
             if(numProcessed>=MAX_VARIANTS): break
-    n=len(sims)
-    #print(sims)
+    print("NumPools\tDropout")
     for numPools in range(MAX_POOLS):
         ave=statistics.mean([probs[numPools] for probs in sims])
-        print(numPools,ave,"\t")
+        print(numPools+1,round(ave,5),sep="\t")
 
+def getAF(genotypes):
+    A=countAlts(genotypes)
+    N=2*len(genotypes)
+    return float(A)/float(N)
+        
 def getDropoutProbs(genotypes,NUM_DONORS,MAX_POOLS,COVERAGE):
     probs=[]
     for numPools in range(1,MAX_POOLS+1):
@@ -46,16 +55,19 @@ def countAlts(pool):
     return sum([gt[0]+gt[1] for gt in pool])
 
 def getDropout(pools,COVERAGE):
+    if(DEBUG): print("NPOOLS=",len(pools))
     product=1
     for pool in pools:
-        N=len(pool)*2
-        A=countAlts(pool)
-        freq=float(A)/float(N)
-        if(freq>1):
-            raise Exception("freq>1: ",A,N,pool)
-        p=binom.pmf(0,N,freq)
+        N=2*len(pool)
+        freq=getAF(pool)
+        p=binom.pmf(0,COVERAGE,freq)
+        #print("binom(0,",N,",",freq,")=",p)
+        if(DEBUG):
+            print("\tAF=",freq,"A=",countAlts(pool),"N=",N,sep="\t",end="")
+            print("\tbinom=",p)
         if(math.isnan(p)): raise Exception("NaN in binom(",0,N,freq)
         product*=p
+    if(DEBUG): print("\tprod=",product)
     return product
 
 def partition(genotypes,NUM_POOLS):
@@ -76,20 +88,22 @@ def processLine(line,NUM_DONORS):
         GT.append(gt)
     return GT
             
-#CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  HG00096
-
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=6):
-    exit(ProgramName.get()+" <in.vcf.gz> <max-pools> <max-variants> <#donors> <coverage>\nwhere coverage is the average # fragments per position\n")
-(vcfFilename,MAX_POOLS,MAX_VARIANTS,NUM_DONORS,COVERAGE)=sys.argv[1:]
+if(len(sys.argv)!=8):
+    exit(ProgramName.get()+" <in.vcf.gz> <max-pools> <max-variants> <#donors> <coverage> <min-AF> <max-AF>\nwhere coverage is the average # fragments per position\n")
+(vcfFilename,MAX_POOLS,MAX_VARIANTS,NUM_DONORS,COVERAGE,
+ MIN_AF,MAX_AF)=sys.argv[1:]
 MAX_POOLS=int(MAX_POOLS)
 MAX_VARIANTS=int(MAX_VARIANTS)
 NUM_DONORS=int(NUM_DONORS)
 COVERAGE=int(COVERAGE)
+MIN_AF=float(MIN_AF)
+MAX_AF=float(MAX_AF)
 
-processVCF(vcfFilename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE)
+processVCF(vcfFilename,MAX_VARIANTS,NUM_DONORS,MAX_POOLS,COVERAGE,
+           MIN_AF,MAX_AF)
 
 
 
